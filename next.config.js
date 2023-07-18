8cad975892545cbb0cef60b21e1c0c250709af9c
Cristian Garcia <garcia.ac.cristian@gmail.com>
2023-07-18 19:40:59 -0400

    projetos-linkados

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dummyimage.com",
-        port: "",
-        pathname: "/720x600/F3F4F7/8693ac",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/app/components/Projetos/Projetos.jsx
+++ src/app/components/Projetos/Projetos.jsx
@@ -14,7 +14,7 @@ const Projetos = () => {
           <div className="mb-4 w-full md:w-1/2 lg:w-1/3 lg:max-w-lg">
             <Image
               alt="hero"
-              src="https://dummyimage.com/720x600/F3F4F7/8693ac"
+              src="/images/vibess.json"
               width={400}
               height={400}
               className="rounded-lg object-cover object-center "
@@ -32,13 +32,20 @@ const Projetos = () => {
             </p>
             <div className="flex flex-col justify-center lg:flex-row">
               <button className="focus:shadow-outline mt-auto flex transform items-center rounded-lg bg-[#3d717a] px-6 py-2 font-semibold text-white ring-offset-2 ring-offset-current transition duration-500 ease-in-out hover:bg-[#1f2937] focus:outline-none focus:ring-2">
-                <a href="">Acessar</a>
+                <a
+                  href="https://vibess-dev-garcia.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Acessar
+                </a>
               </button>
               <p className="mt-2 text-left text-sm md:ml-6 md:mt-0">
                 <a
-                  href="#"
+                  href="https://github.com/dev-garcia/vibess"
+                  target="_blank"
+                  rel="noreferrer noopener"
                   className="inline-flex items-center font-semibold text-[#e45250] hover:text-black md:mb-2 lg:mb-0 "
-                  title="read more"
                 >
                   leve-me ao repositório.
                 </a>
@@ -50,7 +57,7 @@ const Projetos = () => {
         <div className="container mx-auto  flex flex-col items-center px-5 py-16 md:flex-row lg:px-28">
           <div className="mb-4 w-full md:w-1/2 lg:w-1/3 lg:max-w-lg">
             <Image
-              src="https://dummyimage.com/720x600/F3F4F7/8693ac"
+              src="/images/ref-color.json"
               alt="hero"
               width={400}
               height={400}
@@ -67,13 +74,62 @@ const Projetos = () => {
             </p>
             <div className="flex flex-col justify-center lg:flex-row">
               <button className="focus:shadow-outline mt-auto flex transform items-center rounded-lg bg-[#3d717a] px-6 py-2 font-semibold text-white ring-offset-2 ring-offset-current transition duration-500 ease-in-out hover:bg-[#1f2937] focus:outline-none focus:ring-2">
-                <a href="">Acessar</a>
+                <a
+                  href="https://ref-color-dev-garcia.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Acessar
+                </a>
               </button>
               <p className="mt-2 text-left text-sm md:ml-6 md:mt-0">
                 <a
-                  href="#"
+                  href="https://github.com/dev-garcia/Ref-Color"
+                  target="_blank"
+                  rel="noreferrer noopener"
                   className="inline-flex items-center font-semibold text-[#e45250] hover:text-black md:mb-2 lg:mb-0 "
-                  title="read more"
+                >
+                  leve-me ao repositório.
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="container mx-auto  flex flex-col items-center px-5 py-16 md:flex-row lg:px-28">
+          <div className="mb-4 w-full md:w-1/2 lg:w-1/3 lg:max-w-lg">
+            <Image
+              src="/images/saude-em-numeros.json"
+              alt="hero"
+              width={400}
+              height={400}
+              className="rounded-lg object-cover object-center "
+            />
+          </div>
+          <div className="flex flex-col items-center text-left sm:ml-12 sm:items-start   md:mb-0 md:w-1/2 md:pr-16 lg:flex-grow lg:pr-24">
+            <h2 className="mb-4 text-left text-2xl font-black tracking-tighter text-black md:text-5xl">
+              Saúde em Números
+            </h2>
+            <p className="mb-4 text-left text-base leading-relaxed ">
+              Neste projeto disponibilzei a calculadora ICM e uma para brincar
+              com a idade.
+            </p>
+            <div className="flex flex-col justify-center lg:flex-row">
+              <button className="focus:shadow-outline mt-auto flex transform items-center rounded-lg bg-[#3d717a] px-6 py-2 font-semibold text-white ring-offset-2 ring-offset-current transition duration-500 ease-in-out hover:bg-[#1f2937] focus:outline-none focus:ring-2">
+                <a
+                  href="https://saude-em-numeros.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Acessar
+                </a>
+              </button>
+              <p className="mt-2 text-left text-sm md:ml-6 md:mt-0">
+                <a
+                  href="https://github.com/dev-garcia/saude-em-numeros"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="inline-flex items-center font-semibold text-[#e45250] hover:text-black md:mb-2 lg:mb-0 "
                 >
                   leve-me ao repositório.
                 </a>
