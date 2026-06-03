917b95cb7ce1c8c9c430539d7f41850a75dfbbbf
dev-garcia <garcia.ac.cristian@gmail.com>
2026-06-03 16:26:25 -0400

    chore: add shadcn, update dependencies and improve styling
    - Adds updated package dependencies in package.json, including @base-ui/react, @gsap/react, and shadcn.
    - Updated package dependencies in package.json, react and next.js.
    - Change react-icons to lucide-react.
    - Enhanced global styles in globals.css with new color variables and imported additional CSS libraries.
    - Refactored layout.tsx to include Google Fonts and improved class management with a utility function.
    - Added utility functions for class name merging in utils.ts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,22 @@
 import type { Metadata } from "next";
+import { DM_Sans, DM_Serif_Display } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  axes: ["opsz"],
+  display: "swap",
+  variable: "--font-sans",
+});
+
+const dmSerif = DM_Serif_Display({
+  subsets: ["latin"],
+  weight: "400",
+  style: ["normal", "italic"],
+  display: "swap",
+  variable: "--font-heading",
+});
 
 export const metadata: Metadata = {
   title: "Cristian Garcia",
@@ -12,8 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className="font-sans antialiased">{children}</body>
+    <html
+      lang="pt-BR"
+      className={cn(
+        "h-full",
+        "scroll-smooth",
+        "antialiased",
+        "font-sans",
+        dmSans.variable,
+        dmSerif.variable,
+      )}
+    >
+      <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
 }
